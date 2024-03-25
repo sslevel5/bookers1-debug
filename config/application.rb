d886c958174c87e8dd624341259e367dbf67b820
EC2 Default User <ec2-user@ip-172-31-16-147.ap-northeast-1.compute.internal>
2024-03-25 11:25:02 +0000

    application.rb_20240325

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Bookers
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-    
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +18,6 @@ module Bookers
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.paths.add 'lib', eager_load: true
   end
 end
